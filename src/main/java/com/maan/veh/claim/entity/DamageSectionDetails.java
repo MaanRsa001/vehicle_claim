1029ed5a8e408550a2e0ae4336cdb8e7d04e69ad
Nithese <89253083+MaanRsa001@users.noreply.github.com>
2024-08-31 10:50:40 +0530

    entity change

--- src/main/java/com/maan/veh/claim/entity/DamageSectionDetails.java
+++ src/main/java/com/maan/veh/claim/entity/DamageSectionDetails.java
@@ -19,74 +19,74 @@ import lombok.NoArgsConstructor;
 public class DamageSectionDetails {
 
     @Id
-    @Column(name = "claim_no", length = 50)
+    @Column(name = "claim_no")
     private String claimNo;
 
     @Id
     @Column(name = "damage_sno")
     private int damageSno;
 
-    @Column(name = "damage_direction", length = 100)
+    @Column(name = "damage_direction")
     private String damageDictDesc;
 
-    @Column(name = "damage_part", length = 100)
+    @Column(name = "damage_part")
     private String damagePart;
 
-    @Column(name = "repair_replace", length = 50)
+    @Column(name = "repair_replace")
     private String repairReplace;
 
     @Column(name = "no_of_parts")
     private Integer noOfParts;
 
-    @Column(name = "garage_price", precision = 10, scale = 2)
+    @Column(name = "garage_price")
     private Double garagePrice;
 
-    @Column(name = "dealer_price", precision = 10, scale = 2)
+    @Column(name = "dealer_price")
     private Double dealerPrice;
 
-    @Column(name = "garage_loginId", length = 50)
+    @Column(name = "garage_loginId")
     private String garageLoginId;
 
-    @Column(name = "dealer_loginId", length = 50)
+    @Column(name = "dealer_loginId")
     private String dealerLoginId;
 
     @Column(name = "surveyor_id")
     private Integer surveyorId;
 
-    @Column(name = "replace_cost", precision = 10, scale = 2)
+    @Column(name = "replace_cost")
     private Double replaceCost;
 
-    @Column(name = "replace_cost_deduct", precision = 10, scale = 2)
+    @Column(name = "replace_cost_deduct")
     private Double replaceCostDeduct;
 
-    @Column(name = "sparepart_deprection", precision = 10, scale = 2)
+    @Column(name = "sparepart_deprection")
     private Double sparepartDeprection;
 
-    @Column(name = "discount_sparepart", precision = 10, scale = 2)
+    @Column(name = "discount_sparepart")
     private Double discountSparepart;
 
-    @Column(name = "totamt_replace", precision = 10, scale = 2)
+    @Column(name = "totamt_replace")
     private Double totamtReplace;
 
-    @Column(name = "labour_cost", precision = 10, scale = 2)
+    @Column(name = "labour_cost")
     private Double labourCost;
 
-    @Column(name = "labour_cost_deduct", precision = 10, scale = 2)
+    @Column(name = "labour_cost_deduct")
     private Double labourCostDeduct;
 
-    @Column(name = "labour_disc", precision = 10, scale = 2)
+    @Column(name = "labour_disc")
     private Double labourDisc;
 
-    @Column(name = "totamt_of_labour", precision = 10, scale = 2)
+    @Column(name = "totamt_of_labour")
     private Double totamtOfLabour;
 
-    @Column(name = "tot_price", precision = 10, scale = 2)
+    @Column(name = "tot_price")
     private Double totPrice;
 
     @Column(name = "entry_date")
     private Date entryDate;
 
-    @Column(name = "status", length = 20)
+    @Column(name = "status")
     private String status;
 }
 
